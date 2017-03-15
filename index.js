f61782d37911f68f7f3173cb819878bcc8237465
Rajesh Nagireddy <rajesh.nagireddy@practo.com>
2017-03-15 14:47:21 +0530

    triggered email for lowest bitcoin

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+
 var express = require('express')
 var app = express()
 var cron = require('cron');
 var request = require('request');
+var mailgun = require('mailgun-js');
 var lowestBitcoin = 1000000;
+var alertPrice = 88000;
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -23,8 +26,30 @@ function checkBitCoinPrice() {
 	    		lowestBitcoin = body.buy;
 	    		console.log('updated --> ' +  body.buy);
 	    	}
+
+	    	if(body.buy < alertPrice) {
+	    		alertPrice-=500;
+	    		sendMail();
+	    	}
 	    }
 	})
+}
+
+function sendMail() {
+	var api_key = 'key-f6e2d6584d6c0224aa5618cb081041d3';
+	var domain = 'sandboxb93db37f6137404d895582a2f8e399ca.mailgun.org';
+	var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+	 
+	var data = {
+	  from: 'Bit coin Alert <bitalert@alert.mailgun.org>',
+	  to: 'rajesh.impalerts@gmail.com',
+	  subject: "Bit Coin Lowest Price " + lowestBitcoin,
+	  text: 'Lowest Bitcoin as of now is ' + lowestBitcoin
+	};
+
+	mailgun.messages().send(data, function (error, body) {
+	  console.log(body);
+	});
 }
 
 var job1 = new cron.CronJob({
